df81f00b108fdb5b7a79475f5379e862aef76be0
moisesfa <moifdez@gmail.com>
2015-06-09 11:13:51 +0200

    Primera pregunta

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// importar paquetes con middlewares
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,10 +6,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// importar enrutadores
 var routes = require('./routes/index');
 
+// crear aplicacion
 var app = express();
 
+// instalar generador de vistas EJS
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -16,14 +20,17 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
+// instalar middlewares
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// instalar enrutadores, asociar rutas a sus gestores 
 app.use('/', routes);
 
+// Resto de rutas genera error 404 de HTTP
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -35,6 +42,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+// Gestion de errores durante el desarollo 
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -47,6 +55,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
+// Gestion de errores de produccion 
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
