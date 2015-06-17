4343a2658eb6fe4c48672c4451e9e66d78fb2857
moisesfa <moifdez@gmail.com>
2015-06-17 13:04:41 +0200

    busq1

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,10 +15,19 @@ exports.load = function(req, res, next, quizId) {
 
 
 //GET /quizes
-exports.index = function(req, res) {
+exports.index = function(req, res) {	
+ if(req.query.search) {
+    var filtro  = (req.query.search || '').replace(" ", "%");
+    models.Quiz.findAll({where:["pregunta like ?", '%'+filtro+'%'],
+    	order:'pregunta ASC'}).then(function(quizes){
+     	res.render('quizes/index', {quizes: quizes});
+    }).catch(function(error) { next(error);});
+
+  } else {
 	models.Quiz.findAll().then(function(quizes){
 		res.render('quizes/index.ejs', { quizes: quizes});		
 	}).catch(function(error){ next (error);});	
+	}
 };
 
 //GET /quizes/:id
