bfbb8575bb7a8d55769b49aa838732842338c6a6
moisesfa <moifdez@gmail.com>
2015-06-17 12:16:41 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,29 +1,38 @@
 // El controlador importa el modelo para poder acceder a la DB
 var models = require('../models/models.js');
 
-//GET /quizes/:id
-exports.show = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', { quiz: quiz});		
-	})	
-};
-
-//GET /quizes/:id/answer
-exports.answer = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta === quiz.respuesta){
-		res.render('quizes/answer', {quiz:quiz, respuesta: 'Correcto'});		
+// Autoload - factoriza el codigo si ruta incluye : quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(function(quiz){
+		if (quiz) {
+			req.quiz = quiz;
+			next();			
 		} else {
-		res.render('quizes/answer', {quiz:quiz, respuesta: 'Incorrecto'});		
-		}		
-	})
-};
+			next(new Error('No existe quizId=' + quizId));
+		}
+	}).catch(function (error){next (error);});
+}; 
+
 
 //GET /quizes
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes){
 		res.render('quizes/index.ejs', { quizes: quizes});		
-	})	
+	}).catch(function(error){ next (error);});	
+};
+
+//GET /quizes/:id
+exports.show = function(req, res) {
+		res.render('quizes/show', { quiz: req.quiz});		
+};
+
+//GET /quizes/:id/answer
+exports.answer = function(req, res){
+	var resultado = 'Incorrecto';
+		if (req.query.respuesta === req.quiz.respuesta){
+		 resultado = 'Correcto';
+		} 
+		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});		
 };
 
 //GET /author
