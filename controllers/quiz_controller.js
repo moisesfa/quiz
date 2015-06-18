77449b7ca1aa0f7129ab0ee388e7d6722b81795a
moisesfa <moifdez@gmail.com>
2015-06-18 17:07:59 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -88,6 +88,13 @@ exports.update = function(req, res) {
 	});
 };
 
+// DELETE /quizes/:id
+exports.destroy = function(req, res){
+	req.quiz.destroy().then(function(){
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+
+};
 //GET /author
 exports.author = function(req, res) {
 	res.render('author', {author: 'Moisés Fernández Arias',errors:[]});	
