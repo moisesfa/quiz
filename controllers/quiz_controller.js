b77018124d2546d38457fac4a3713ba549ad14a6
moisesfa <moifdez@gmail.com>
2015-06-18 13:30:25 +0200

    Validacion entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,26 +13,25 @@ exports.load = function(req, res, next, quizId) {
 	}).catch(function (error){next (error);});
 }; 
 
-
 //GET /quizes
 exports.index = function(req, res) {	
  if(req.query.search) {
     var filtro  = (req.query.search || '').replace(" ", "%");
     models.Quiz.findAll({where:["pregunta like ?", '%'+filtro+'%'],
     	order:'pregunta ASC'}).then(function(quizes){
-     	res.render('quizes/index', {quizes: quizes});
+     	res.render('quizes/index', {quizes: quizes,errors:[]});
     }).catch(function(error) { next(error);});
 
   } else {
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', { quizes: quizes});		
+		res.render('quizes/index.ejs', { quizes: quizes,errors:[]});		
 	}).catch(function(error){ next (error);});	
 	}
 };
 
 //GET /quizes/:id
 exports.show = function(req, res) {
-		res.render('quizes/show', { quiz: req.quiz});		
+		res.render('quizes/show', { quiz: req.quiz,errors:[]});		
 };
 
 //GET /quizes/:id/answer
@@ -41,29 +40,34 @@ exports.answer = function(req, res){
 		if (req.query.respuesta === req.quiz.respuesta){
 		 resultado = 'Correcto';
 		} 
-		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});		
+		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado,errors:[]});		
 };
 
 //GET /quizes/new 
 exports.new = function(req, res) {
 	var quiz = models.Quiz.build(
 		{pregunta: "Pregunta", respuesta: "Respuesta"});
-	res.render('quizes/new', {quiz: quiz});	
+	res.render('quizes/new', {quiz: quiz,errors:[]});	
 };
 
 //POST /quizes/create
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build(req.body.quiz);
-
-	// guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	})  // redirección HTTP (URL relativo) lista de preguntas
+	
+	quiz.validate().then(function(err){
+		if (err) {
+			res.render('quizes/new', {quiz: quiz, errors: err.errors});
+		} else {
+		// guarda en DB los campos pregunta y respuesta de quiz
+		quiz.save({fields: ["pregunta", "respuesta"]}).
+		then(function(){res.redirect('/quizes')})  
+		} // redirección HTTP (URL relativo) lista de preguntas
+	});
 };
 
 //GET /author
 exports.author = function(req, res) {
-	res.render('author', {author: 'Moisés Fernández Arias'});	
+	res.render('author', {author: 'Moisés Fernández Arias',errors:[]});	
 };
 
 /*
--- models/quiz.js
+++ models/quiz.js
@@ -1,10 +1,16 @@
-// Definicion del modelo de Quiz
+// Definicion del modelo de Quiz con validacion
 // El fichero quiz.js es parte del modelo (directorio models) y define 
 // la estructura de la tabla con 2 campos (tipo string)
 
 module.exports = function (sequelize, DataTypes) {
 	return sequelize.define ('Quiz', 
-							{ pregunta: DataTypes.STRING,
-							  respuesta: DataTypes.STRING
+							{ pregunta: {
+								type: DataTypes.STRING,
+								validate: {notEmpty: {msg: "-> Falta Pregunta"}}
+								},
+							  respuesta: {
+							  	type: DataTypes.STRING,
+							  	validate: {notEmpty: {msg: "-> Falta Respuesta"}}
+							  }
 							});
 }
