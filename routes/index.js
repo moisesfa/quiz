6587e63d65891ae4e0597268ee8d781af838df7b
moisesfa <moifdez@gmail.com>
2015-06-25 10:35:07 +0200

    Moderacion comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/', function(req, res) {
 });
 
 //Autoload de comandos con :quizId
-router.param('quizId', quizController.load); 
+router.param('quizId', quizController.load);		// autoload : quizId
+router.param('commentId', commentController.load);  // autoload :commentId 
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -33,5 +34,6 @@ router.get('/author', quizController.author);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
